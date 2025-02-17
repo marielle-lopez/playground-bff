45bb14186af4c81145564dd688953be225a149aa
marielle-lopez <mariellelouisse@gmail.com>
2025-02-18 01:02:22 +1100

    Add implementation for `PATCH` requests

--- src/main/java/mariellelopez/playground/posts/PostService.java
+++ src/main/java/mariellelopez/playground/posts/PostService.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,6 +34,44 @@ public class PostService {
         newPost.setTags(data.getTags());
 
         return this.postRepository.save(newPost);
+    };
+
+    public Optional<Post> updatePostById(Long id, @Valid UpdatePostDTO data) {
+        Optional<Post> foundPost = this.postRepository.findById(id);
+
+        if (foundPost.isEmpty()) {
+            return foundPost;
+        };
+
+        Post updatedPost = foundPost.get();
+
+        if (data.getTitle() != null) {
+            updatedPost.setTitle(data.getTitle());
+        };
+
+        if (data.getAuthor() != null) {
+            updatedPost.setAuthor(data.getAuthor());
+        };
+
+        if (data.getIntroduction() != null) {
+            updatedPost.setIntroduction(data.getIntroduction());
+        };
+
+        if (data.getBody() != null) {
+            updatedPost.setBody(data.getBody());
+        };
+
+        if (data.getConclusion() != null) {
+            updatedPost.setConclusion(data.getConclusion());
+        };
+
+        if (data.getTags() != null) {
+            updatedPost.setTags(data.getTags());
+        };
+
+        updatedPost.setModifiedAt(new Date());
+
+        return Optional.of(updatedPost);
     };
 
     public Boolean deletePostById(Long id) {
