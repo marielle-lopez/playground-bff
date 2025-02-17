1985440b951546ed809a6b2ace0dfd837f862222
marielle-lopez <mariellelouisse@gmail.com>
2025-02-17 23:21:58 +1100

    Implement methods for `GET`, `POST`, and `DELETE` requests

--- src/main/java/mariellelopez/playground/posts/PostService.java
+++ src/main/java/mariellelopez/playground/posts/PostService.java
@@ -4,20 +4,45 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class PostService {
     @Autowired
     private PostRepository postRepository;
 
-    public String getAllPosts() {
-        return "This gets all of the posts.";
+    public List<Post> getAllPosts() {
+        return this.postRepository.findAll();
     };
 
-    public String getPostById(int id) {
-        return "This gets the post with ID: " + id + ".";
+    public Optional<Post> getPostById(Long id) {
+        return this.postRepository.findById(id);
     };
 
-    public String createPost(@Valid CreatePostDTO data) {
-        return "This creates a post with the title: " + data.getTitle() + ".";
+    public Post createPost(@Valid CreatePostDTO data) {
+        Post newPost = new Post();
+
+        newPost.setTitle(data.getTitle());
+        newPost.setCreatedAt(data.getCreatedAt());
+        newPost.setModifiedAt(data.getModifiedAt());
+        newPost.setAuthor(data.getAuthor());
+        newPost.setIntroduction(data.getIntroduction());
+        newPost.setBody(data.getBody());
+        newPost.setConclusion(data.getConclusion());
+        newPost.setTags(data.getTags());
+
+        return this.postRepository.save(newPost);
+    };
+
+    public Boolean deletePostById(Long id) {
+        Optional<Post> foundPost = this.postRepository.findById(id);
+
+        if (foundPost.isEmpty()) {
+            return false;
+        }
+
+        this.postRepository.delete(foundPost.get());
+        return true;
     };
 }
